02d7c87ac084c8ae32a2e7e49c88e4de2d05d124
JenelleNg <24013515@myrp.edu.sg>
2026-01-14 14:03:02 +0800

    updated app.js

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const myurl = 'https://onlinecardappwebservice-o2ua.onrender.com/allcards';
+let originalData = [];
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    const [mydata, setMydata] = useState([]);
+
+    useEffect(() => {
+        fetch(myurl)
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                setMydata(myJson);
+                originalData = myJson;
+            });
+    }, []);
+
+    const FilterData = (text) => {
+        if (text != '') {
+            let myFilteredData = originalData.filter((item) =>
+                item.card_name.toLowerCase().includes(text.toLowerCase())
+                //basically changing the search and card_name into all lower case so even if you have Uppercase in the search, it will convert into lowercase to find matching name.
+            );
+            setMydata(myFilteredData);
+        } else {
+            setMydata(originalData);
+        }
+    };
+
+    const renderItem = ({ item }) => {
+        return (
+            <View
+                style={{
+                    borderWidth: 1,
+                    padding: 10,
+                    marginVertical: 5,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}
+            >
+                <Text style={{ flex: 1, fontWeight: 'bold' }}> {item.card_name}</Text>
+
+                <Image
+                    source={{ uri: item.card_pic }}
+                    style={{ width: 180, height: 210 }}
+                    resizeMode="contain"
+                />
+            </View>
+        );
+    };
+    return (
+        <View style={{ padding: 10 }}>
+            <StatusBar />
+            <Text>Search:</Text>
+
+            <TextInput
+                style={{ borderWidth: 1, marginBottom: 10 }}
+                onChangeText={(text) => {
+                    FilterData(text);
+                }}
+            />
+
+            <FlatList
+                data={mydata}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+    );
+};
+
+export default App;
